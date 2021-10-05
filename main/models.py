69562368f3bdd24789421fb55848f14308bff4a7
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-10-05 14:02:10 +0300

    created model Group which is the logic SuperGrp and SubGrp

--- main/models.py
+++ main/models.py
@@ -3,11 +3,45 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class Group(models.Model):
-    title = models.CharField(unique=True, max_length=12, verbose_name='Название группы')
-    super_group = models.ForeignKey()
+    title = models.CharField(unique=True, max_length=122, verbose_name='Название группы')
+    super_group = models.ForeignKey('SuperGroup', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Группа')
 
 
 class SuperGroupManager(models.Manager):
-    pass
+    def get_queryset(self):
+        return super().get_queryset().filter(super_group__isnull=True)
+
+
+class SuperGroup(Group):
+    objects = SuperGroupManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        proxy = True
+        ordering = ('title', )
+        verbose_name = ' Над группа'
+        verbose_name_plural = 'Над группы'
+
+
+class SubGroupManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_group__isnull=False)
+
+
+class SubGroup(Group):
+    objects = SuperGroupManager()
+
+    def __str__(self):
+        return f'{self.super_group.name} - {self.title}'
+
+    class Meta:
+        proxy = True
+        ordering = ('super_group__name', 'title')
+        verbose_name = 'Под группа'
+        verbose_name_plural = 'Под группы'
+
+
 
 
