c8de4f96f5011f094bbf94d49b4d84d4599c11a5
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-10-07 18:43:15 +0300

    added router for ListStudents

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import *
+from .views import RegisterUserApiView, StudentsApiView, StudentsViewSet
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+
+router.register('user_list', StudentsViewSet)
+
 app_name = 'main'
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('register/', RegisterUserApiView.as_view(), name='register_user'),
     path('user_list/', StudentsApiView.as_view(), name='user_list'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,6 +9,8 @@ from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework import permissions
 
 
 class RegisterUserApiView(APIView):
@@ -19,9 +21,15 @@ class RegisterUserApiView(APIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
 
 
 class StudentsApiView(ListAPIView):
     queryset = Student.object.all()
     serializer_class = StudentsSerializer
+
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    queryset = Student.object.all()
+    serializer_class = StudentsSerializer
+    permission_classes = [permissions.IsAuthenticated]
