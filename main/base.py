fc6c4c89da7f9413fed8a67de9926c4f8e466228
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-12-17 13:44:35 +0200

    linking database queries and web-service

--- main/base.py
+++ main/base.py
@@ -1,8 +1,12 @@
 import time
 from contextlib import contextmanager
+from threading import local
 
+from django.utils.encoding import force_text
 from django.db.backends.postgresql.base import DatabaseWrapper as DjangoDatabaseWrapper
 from django.db.backends.utils import CursorWrapper as DjangoCursorWrapper
+
+thread_local = local()
 
 
 @contextmanager
@@ -14,8 +18,16 @@ def calc_sql_time(sql):
     print(f'Продолжительность SQL-запроса{sql} - {time.monotonic() - timestamp:.3f} сек.')
 
 
+def make_safe(s):
+    return s.replace('*', '').replace('\\', '').replace('%', '')
+
+
 class CursorWrapper(DjangoCursorWrapper):
     def execute(self, sql, params=None):
+        path = getattr(thread_local, 'path', '')
+        if path:
+            path = make_safe(path)
+            sql = f'/* {path} */\n{force_text(sql)}\n/* {path} */'
         with calc_sql_time(sql):
             return super().execute(sql, params)
 
--- main/middlewares.py
+++ main/middlewares.py
@@ -1,5 +1,6 @@
 from .models import SubGroup
 import time
+from .base import thread_local
 
 
 def group_context_processor(request):
@@ -13,11 +14,12 @@ class RequestTimeMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        thread_local.path = request.path
         timestamp = time.monotonic()
 
         response = self.get_response(request)
 
         print(f'Продолжительность запроса {request.path} - {time.monotonic() - timestamp:.3f} sec.')
 
+        thread_local.path = ''
         return response
-
