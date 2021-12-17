ec02c8a1dea970e75a09871a2f217f77acb0c943
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-12-17 11:11:29 +0200

    added middleware for couting request

--- main/middlewares.py
+++ main/middlewares.py
@@ -1,7 +1,23 @@
 from .models import SubGroup
+import time
 
 
 def group_context_processor(request):
     context = {}
     context['group'] = SubGroup.objects.all()
     return context
+
+
+class RequestTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        timestamp = time.monotonic()
+
+        response = self.get_response(request)
+
+        print(f'Продолжительность запроса {request.path} - {time.monotonic() - timestamp:.3f} sec.')
+
+        return response
+
