bd46f5d2e2d147e2a2d553769e593227bbb2fb76
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2021-12-17 11:52:21 +0200

    added file base.py(mechanism engine), create db(postgresql)

--- school/settings.py
+++ school/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'main',
+        'NAME': env('POSTGRE_DB'),
+        'USER': env('POSTGRE_USER'),
+        'PASSWORD': env('POSTGRE_PASSWORD'),
+        'HOST': env('POSTGRE_HOST'),
+        'PORT': env('POSTGRE_PORT'),
     }
 }
 
